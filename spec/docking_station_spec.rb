c45d73236c210e9e2dfa334186ae00a9826cb638
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-11-12 15:33:20 +0000

    added new test to respond to release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,4 +2,8 @@ require "docking_station"
 
 describe DockingStation do
 
+  it "releases bike" do
+    docking_station = DockingStation.new
+    docking_station.responds_to(release_bike)
+  end
 end
