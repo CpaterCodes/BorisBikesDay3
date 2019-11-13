b4c0479822adc3e0457604ea00ad70b26d623078
Charlie Paterson <Charlieed.paterson@googlemail.com>
2019-11-13 15:59:37 +0000

    Created and tested a docking function using an attr_reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,25 @@
 class DockingStation
+attr_reader :bike_stored
+
+  def initialize
+    @bike_stored = []
+  end
+
+def see
+  if @bike_stored.length == 0
+    puts 'Empty :/'
+    return false
+  else
+    puts 'Bike! :D'
+    return true
+  end
+end
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @bike_stored << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,3 +5,16 @@ describe 'DockingStation' do
     it { expect(DockingStation.new.release_bike).to be_instance_of(Bike) }
     it { expect(DockingStation.new.release_bike.working?).to eq true }
 end
+
+describe 'DockingStation' do
+  it { expect(DockingStation.new).to respond_to(:dock_bike) }
+end
+
+describe 'DockingStation' do
+  station = DockingStation.new
+  bike = Bike.new
+  station_2 = DockingStation.new
+  station_2.dock_bike(bike)
+  it { expect(station.see).to eq false }
+  it { expect(station_2.see).to eq true }
+end
