d777c60a7b5f3697c24e3ffc6e5642bda8609106
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-11-14 16:45:11 +0000

    test and implement bike.break and prevent broken bikes from being released

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,12 @@
 require "bike"
 
-describe 'Bike' do
+describe Bike do
     it { expect(Bike.new).to respond_to(:working?) }
+
+    it "should report when it is broken" do
+        test_bike = Bike.new
+        test_bike.break
+        expect(test_bike.working?).to be false
+    end
+
 end
